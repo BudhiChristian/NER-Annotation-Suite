bb45466ff6dcaf085155092ac0255b598cc4080b
BudhiChristian <cjbudhi95@gmail.com>
2020-04-21 11:51:42 -0400

    Setup routes and simple navigation layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { AnnotationComponent } from './components/annotation/annotation.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  }, {
+    path: 'about',
+    component: AboutComponent
+  }, {
+    path: 'annotate',
+    component: AnnotationComponent
+  }, {
+    path: 'documentation',
+    component: DocumentationComponent
+  }, {
+    path: '**',
+    redirectTo: 'about'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AnnotationComponent } from './components/annotation/annotation.component';
+import { DocumentationComponent } from './components/documentation/documentation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
+
+const MATERIAL_IMPORTS = [MatToolbarModule, MatButtonModule]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AnnotationComponent,
+    DocumentationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_IMPORTS
   ],
   providers: [],
   bootstrap: [AppComponent]
